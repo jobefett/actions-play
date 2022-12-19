94e2c9f06ad704f4c419143f1a8b785363ae8620
jobefett <91795467+jobefett@users.noreply.github.com>
2022-12-20 00:49:59 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const cart = [];
+
+function addToCart(item) {
+  cart.push(item);
+  console.log("added item: " + item);
+}
+
+function removeFromCart(idx) {
+  cart.splice(idx, 1);
+    console.log("removed: ", idx);
+}
+
+addToCart("Waterproof Boots");
