964637b4dbcaf2bb614f1092f5320373e6edfc89
jobefett <91795467+jobefett@users.noreply.github.com>
2022-12-20 02:11:37 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 const cart = [];
 
-function addToCart(item) {
+function addToCart (item) 
+{  
   cart.push(item);
   console.log("added item: " + item);
 }
 
 function removeFromCart(idx) {
   cart.splice(idx, 1);
-    console.log("removed: ", idx);
+    console.log("removed: ", id x);
 }
 
 addToCart("Waterproof Boots");
+//
